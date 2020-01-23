416afff77dd352d6c93d021b1d50f437f0d4e28c
Kris Johnson <krisrjohnson@gmail.com>
2020-01-23 15:01:04 -0500

    initial commit

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,31 @@
+class HousesController < ApplicationController
+  def index
+    @houses = House.all
+  end
+
+  def show
+    @house = House.find(params["id"])
+    @members = @house.members
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+
+    if @house.save
+      flash[:notice] = "House added successfully"
+      redirect_to @house
+    else
+      flash.now[:error] = @house.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+    def house_params
+      params.require("house").permit("name", "source", "author", "motto")
+    end
+end
---
+++ db/migrate/20200123184323_create_members.rb
@@ -0,0 +1,12 @@
+class CreateMembers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :members do |t|
+      t.belongs_to :house, null: false
+
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+
+      t.timestamps
+    end
+  end
+end
